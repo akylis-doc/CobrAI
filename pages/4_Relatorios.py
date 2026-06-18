aaf5ee9d06a75f1b061a5a929d5298902b9d1a6c
akylis-doc <nicollasadc@gmail.com>
2026-06-18 10:12:21 -0300

    Update 4_Relatorios.py

--- pages/4_Relatorios.py
+++ pages/4_Relatorios.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="CobraAI - Relatórios", page_icon="📈", layout=
 if st.button("🏠 Voltar para a Home"): 
     st.switch_page("app.py")
 
-st.sidebar.title("💰 CobraAI")
+st.sidebar.title("🐍 CobrAI")
 st.sidebar.text_input("Chave API Gemini", type="password", key="gemini_api_key")
 
 st.title("📈 Relatórios Analytics")
