d0bacf9f15ca0a7b45289654054e60ed21c0da28
akylis-doc <nicollasadc@gmail.com>
2026-06-18 10:12:04 -0300

    Update 2_Cobrancas.py

--- pages/2_Cobrancas.py
+++ pages/2_Cobrancas.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="CobraAI - Cobranças", page_icon="💸", layout="
 if st.button("🏠 Voltar para a Home"):
     st.switch_page("app.py")
 
-st.sidebar.title("💰 CobraAI")
+st.sidebar.title("🐍 CobrAI")
 st.sidebar.text_input("Chave API Gemini", type="password", key="gemini_api_key")
 
 st.title("💸 Gestão de Cobranças")
